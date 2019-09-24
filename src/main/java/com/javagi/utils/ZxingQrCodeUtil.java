1c954c6e5f7ce01de09f598a5a67a47333bcfd5d
wangkui <1272840327@qq.com>
2019-09-24 09:42:46 +0800

    【提交原因】优化导出工具类

--- src/main/java/com/javagi/utils/ZxingQrCodeUtil.java
+++ src/main/java/com/javagi/utils/ZxingQrCodeUtil.java
@@ -6,10 +6,7 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
